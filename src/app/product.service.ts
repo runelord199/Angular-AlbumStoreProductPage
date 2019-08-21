84db77bba5a034cd8f9c9f6411368df2737bd9b4
tankl@hotmail.com <jonathan.kl.tan@avanade.com>
2019-08-21 10:56:06 +0800

    Course 2 Resubmit 3

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -5,10 +5,10 @@ import 'rxjs/add/operator/map'
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json';
-  constructor(private _http:Http) { }
+  constructor(private _http:Http ) { }
   getAlbum(id:number)
   {
-    return this._http.get(this._albumUrl).map((Response) => Response.json());
+    return this._http.get(this._albumUrl).map((response) => response.json());
   }
 
 
