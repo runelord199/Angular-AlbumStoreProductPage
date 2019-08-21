d8a93950f2c4f8207468f353fa36f50eb356aba6
tankl@hotmail.com <jonathan.kl.tan@avanade.com>
2019-08-21 14:48:09 +0800

    Course 6

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,17 +2,22 @@ import { Injectable } from '@angular/core';
 import { Response, Http } from '@angular/http';
 import 'rxjs/add/operator/map'
 
-
+import { Product } from './product';
 import { Observable } from 'rxjs/Observable';
 import { Album } from './album';
 
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json';
+  private _productsUrl : string = '../assets/products.json';
   constructor(private _http:Http ) { }
   getAlbum(id:number): Observable<Album>
   {
     return this._http.get(this._albumUrl).map((response) => <Album>response.json());
+  }
+  getProducts(): Observable<Product>
+  {
+    return this._http.get(this._productsUrl).map((response)=> <Product>response.json());
   }
 
 
